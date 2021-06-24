5215ab3a95596a033c9836955f4c9e35656b5d5b
Jerry20110434 <aa>
2021-06-24 16:12:21 +0800

    added model.py and features.py. added load_training_data() in train.py

--- model.py
+++ model.py
@@ -51,7 +51,7 @@ class GATModel(nn.Module):
         hidden = out[:, -1, :]
         for l in range(self.num_layers_gat):
             hidden = self.gat_layers[l](hidden)
-            hidden = hidden + out[:, -1:, :]
+            hidden = hidden + out[:, -1:, :] # residual connection from initial feature (layer k=0)
         hidden = self.fc(hidden)
         hidden = self.leaky_relu(hidden)
         return self.fc_out(hidden).squeeze()
